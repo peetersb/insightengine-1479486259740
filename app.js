26ea430545618d524665b6252d2b4c6740983e0e
Brendt Peeters <peetersb@schneider.com>
2016-11-18 11:38:48 -0600

    Finishing experiment.

--- app.js
+++ app.js
@@ -56,10 +56,3 @@ require('./config/error-handler')(app);
 var port = process.env.VCAP_APP_PORT || 3000;
 app.listen(port);
 console.log('listening at:', port);
-
-app.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-})
-
